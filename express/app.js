2104bd04f8b18e8321728bda698c27a15eaf4dd3
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-06-09 17:26:39 -0300

    Express - POST routes with form data and json data

--- express/app.js
+++ express/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+// parsers
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // ===== view engine
 app.set('view engine', 'ejs');
@@ -21,7 +26,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/person/:id', (req, res) => {
-    res.render('person', { id: req.params.id });
+    res.render('person', { id: req.params.id, Qstr: req.query.qstr });
+});
+
+app.post('/person', urlencodedParser, (req, res) => {
+    res.send('Thank you!');
+    console.log(req.body.firstname, req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, (req, res) => {
+    res.send('Thank you for the JSON data!');
+    console.log(req.body.firstname, req.body.lastname);
 });
 
 app.get('/api', (req, res) => {
