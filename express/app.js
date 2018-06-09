00e3f86d5e25b00d3585ee8e5c5980a7448bd9d4
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-06-09 16:46:58 -0300

    Express - Using middlewares and static route

--- express/app.js
+++ express/app.js
@@ -1,14 +1,22 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(cookieParser());
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', (req, res, next) => {
+    console.log('Request Url: ' + req.url);
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('<html><head><title>Teste</title></head><body><h1>Hello world!</h1></body></html>')
+    res.send('<html><head><title>Teste</title><link rel="stylesheet" href="/assets/style.css" /></head><body><h1>Hello world!</h1></body></html>');
 });
 
 app.get('/person/:id', (req, res) => {
-    res.send('<html><head><title>Teste</title></head><body><h1>Person: ' + req.params.id + '</h1></body></html>')
+    res.send('<html><head><title>Teste</title></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
 });
 
 app.get('/api', (req, res) => {
