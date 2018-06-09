9904c3591c3dce30c8d8bfbcc8784812ccde1538
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-06-09 03:19:33 -0300

    Server with stream

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ var fs = require('fs');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
-    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-    var message = 'Hello world...';
-    html = html.replace('{Message}', message);
-    res.end(html);
+    var html = fs.createReadStream(__dirname + '/index.htm', 'utf8');
+    html.pipe(res);
 
 }).listen(1337, '127.0.0.1');
