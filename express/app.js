5714b20b9e88bb2245daae9665d35193438fe7ee
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-06-09 16:30:37 -0300

    Express routing with params

--- express/app.js
+++ express/app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('<html><head><title>Teste</title></head><body><h1>Hello world!</h1></body></html>')
 });
 
+app.get('/person/:id', (req, res) => {
+    res.send('<html><head><title>Teste</title></head><body><h1>Person: ' + req.params.id + '</h1></body></html>')
+});
+
 app.get('/api', (req, res) => {
     res.json({ firstname: 'John', lastname: 'Doe' });
 });
